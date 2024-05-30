fe0374c9ee70e454195fc0057076bfbe5f0075ea
anhtuyen0409 <nguyenanhtuyen10a5@gmail.com>
2024-05-30 18:16:14 +0700

    unit test role entity

--- ShopWeb/ShopBackend/src/main/java/com/nguyenanhtuyen/admin/ShopBackendApplication.java
+++ ShopWeb/ShopBackend/src/main/java/com/nguyenanhtuyen/admin/ShopBackendApplication.java
@@ -2,8 +2,10 @@ package com.nguyenanhtuyen.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.nguyenanhtuyen.common.entity", "com.nguyenanhtuyen.admin.user"})
 public class ShopBackendApplication {
 
 	public static void main(String[] args) {
