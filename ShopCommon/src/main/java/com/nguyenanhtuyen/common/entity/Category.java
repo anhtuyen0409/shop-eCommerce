8b4ff39ff8d20d9a717b9f580b590b064a8a3c59
anhtuyen0409 <nguyenanhtuyen10a5@gmail.com>
2024-06-30 17:22:08 +0700

    unit test category

---
+++ ShopCommon/src/main/java/com/nguyenanhtuyen/common/entity/Category.java
@@ -0,0 +1,64 @@
+package com.nguyenanhtuyen.common.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 128, nullable = false, unique = true)
+	private String name;
+	
+	@Column(length = 64, nullable = false, unique = true)
+	private String alias;
+	
+	@Column(length = 128, nullable = false)
+	private String image;
+	
+	private boolean enabled;
+	
+	@OneToOne
+	@JoinColumn(name = "parent_id")
+	private Category parent;
+	
+	@OneToMany(mappedBy = "parent")
+	private Set<Category> children = new HashSet<>();
+	
+	public Category(String name) {
+		this.name = name;
+		this.alias = name;
+		this.image = "default.png";
+	}
+	
+	public Category(String name, Category parent) {
+		this(name);
+		this.parent = parent;
+	}
+	
+	public Category(Integer id) {
+		this.id = id;
+	}
+}
---
+++ ShopWeb/ShopBackend/src/main/java/com/nguyenanhtuyen/admin/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.nguyenanhtuyen.admin.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.nguyenanhtuyen.common.entity.Category;
+
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+
+}
